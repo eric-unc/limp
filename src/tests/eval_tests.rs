97ea1f19ec009f4dec79102ce55b07dedc34b666
Harry Zheng <harry@harryzheng.com>
2021-03-21 19:51:25 +0800

    Add some tests

--- src/tests/eval_tests.rs
+++ src/tests/eval_tests.rs
@@ -19,6 +19,18 @@ fn eval_add_negative(){
 }
 
 #[test]
+fn eval_add_multiple(){
+	let tree = LimpParser::parse(Rule::program, "(+ 1 2 3 4)").unwrap();
+	eval(tree);
+}
+
+#[test]
+fn eval_minus() {
+	let tree = LimpParser::parse(Rule::program, "(- 5 9)").unwrap();
+	eval(tree);
+}
+
+#[test]
 fn eval_with_whitespace(){
 	let tree = LimpParser::parse(Rule::program, "   (+\t5\r\n9)\n").unwrap();
 	eval(tree);
